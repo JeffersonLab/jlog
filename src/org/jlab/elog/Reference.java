47d156a13ef16ddf2fc345a54cb153e113b4b723
ryans <ryans@sftryans.acc.jlab.org>
2012-10-03 15:32:54 -0400

    Updated documentation

--- src/org/jlab/elog/Reference.java
+++ src/org/jlab/elog/Reference.java
@@ -1,11 +1,15 @@
 package org.jlab.elog;
 
 /**
- *
+ * A document reference for use in an electronic log book.
+ * 
  * @author ryans
  */
 public class Reference {
 
+    /**
+     * The reference type.
+     */
     public enum RefType {
 
         LOGBOOK, ATLIS, CTLIS, FELIST, HALIST, HBLIST, HCLIST, HDLIST, INSVACTL, TATL, PSSLOG
@@ -14,15 +18,31 @@ public class Reference {
     private final RefType type;
     private final String id;
     
+    /**
+     * Construct a new Reference with the specified type and id.
+     * 
+     * @param type The type
+     * @param id The id
+     */
     public Reference(RefType type, String id) {
         this.type = type;
         this.id = id;
     }
 
+    /**
+     * Return the reference type.
+     * 
+     * @return The type
+     */
     public RefType getType() {
         return type;
     }
 
+    /**
+     * Return the reference ID.
+     * 
+     * @return The ID
+     */
     public String getId() {
         return id;
     }
