e67e6a65fe23e6c5e801a2a91c1201299414a93e
slominskir <Ryan.Slominski@gmail.com>
2017-02-24 13:16:53 -0500

    Cleanup

--- src/org/jlab/elog/exception/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides the exception classes for an electronic log book client.
- */
-package org.jlab.elog.exception;
--- src/org/jlab/elog/util/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides the utility classes for an electronic log book client.
- */
-package org.jlab.elog.util;
